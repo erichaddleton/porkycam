bf9d86c756d4cb4b0e898a7dfd021f008944c249
Eric Haddleton <erichaddleton42@gmail.com>
2013-06-27 09:19:14 -0500

    Add Existing app files

---
+++ PorkyCam/src/com/example/porkycam/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.porkycam;
+
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    /** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance(){
+	    Camera c = null;
+	    try {
+	        c = Camera.open(); // attempt to get a Camera instance
+	    }
+	    catch (Exception e){
+	        // Camera is not available (in use or does not exist)
+	    }
+	    return c; // returns null if camera is unavailable
+	}
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    public void startCamera(View view) {
+    	Intent intent = new Intent(this, CameraOn.class);
+        startActivity(intent);
+    
+    }
+}
+    
+
