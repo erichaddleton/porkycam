0f324ab0622941ad40afb91bc936ab1121f21ce3
Eric Haddleton <erichaddleton42@gmail.com>
2013-07-07 21:13:48 -0500

    Revert "Revert "-CameraOn, +timer""
    This reverts commit cbfcd5c16d36b5a058706944ea056841bae3896a.

--- PorkyCam/src/com/example/porkycam/MainActivity.java
+++ PorkyCam/src/com/example/porkycam/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.porkycam;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.hardware.Camera;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -8,25 +12,48 @@ import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends Activity {
+	
+	private Timer camTimer;
+	
+	@Override
+	public void onCreate(Bundle MainActivity) {
+		super.onCreate(MainActivity);
+		setContentView(R.layout.activity_main);
+		final MediaPlayer mp = MediaPlayer.create(this, R.raw.camclick.mp3);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    
-    /** A safe way to get an instance of the Camera object. */
-	public static Camera getCameraInstance(){
-	    Camera c = null;
-	    try {
-	        c = Camera.open(); // attempt to get a Camera instance
-	    }
-	    catch (Exception e){
-	        // Camera is not available (in use or does not exist)
-	    }
-	    return c; // returns null if camera is unavailable
+		camTimer = new Timer();
+		camTimer.schedule(new TimerTask() {			
+			@Override
+			public void run() {
+				TimerMethod();
+			}
+			
+		}, 0, 1000);
 	}
-    
+	
+	private void TimerMethod()
+	{
+		//This method is called directly by the timer
+		//and runs in the same thread as the timer.
+
+		//We call the method that will work with the UI
+		//through the runOnUiThread method.
+		this.runOnUiThread(Timer_Tick);
+	}
+	
+	
+	private Runnable Timer_Tick = new Runnable() {
+		public void run() {
+		
+			//This method runs in the same thread as the UI.    	       
+		
+			//Do something to the UI thread here
+			mp.start();	
+		
+	
+		}
+	};
+	
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -35,8 +62,7 @@ public class MainActivity extends Activity {
     }
     
     public void startCamera(View view) {
-    	Intent intent = new Intent(this, CameraOn.class);
-        startActivity(intent);
+    	
     
     }
 }
