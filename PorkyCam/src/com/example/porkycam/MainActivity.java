331fce2cb19b446c4f943e5b362774db144325d5
Eric Haddleton <erichaddleton42@gmail.com>
2013-07-09 14:32:20 -0500

    Added Functional Timer
    Huzzah.

--- PorkyCam/src/com/example/porkycam/MainActivity.java
+++ PorkyCam/src/com/example/porkycam/MainActivity.java
@@ -4,55 +4,40 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import android.hardware.Camera;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 	
-	private Timer camTimer;
+	TimerTask mTimerTask;
+	final Handler handler = new Handler();
+	Timer t = new Timer();
+	TextView hTextView;
+	TableRow hTableRow;
+	Button hButton, hButtonStop;
+	private int nCounter = 0;
+	MediaPlayer _shootMP = null;
 	
 	@Override
-	public void onCreate(Bundle MainActivity) {
-		super.onCreate(MainActivity);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		final MediaPlayer mp = MediaPlayer.create(this, R.raw.camclick.mp3);
-
-		camTimer = new Timer();
-		camTimer.schedule(new TimerTask() {			
-			@Override
-			public void run() {
-				TimerMethod();
-			}
-			
-		}, 0, 1000);
+		hTextView = (TextView)findViewById(R.id.textView2);
+		hButton = (Button)findViewById(R.id.toggleButton1);
 	}
-	
-	private void TimerMethod()
-	{
-		//This method is called directly by the timer
-		//and runs in the same thread as the timer.
-
-		//We call the method that will work with the UI
-		//through the runOnUiThread method.
-		this.runOnUiThread(Timer_Tick);
-	}
-	
-	
-	private Runnable Timer_Tick = new Runnable() {
-		public void run() {
-		
-			//This method runs in the same thread as the UI.    	       
-		
-			//Do something to the UI thread here
-			mp.start();	
-		
-	
-		}
-	};
 	
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -61,10 +46,66 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    public void startCamera(View view) {
-    	
-    
+    public void onToggleClick(View view){
+        // Is the toggle on?
+        boolean on = ((ToggleButton) view).isChecked();
+        if (on) {
+            doTimerTask();
+        } else {
+            stopTask();
+        }
     }
-}
+    public void doTimerTask()
+    {
+    	Log.d("TIMER","TimerTask Started");
+    	
+    	mTimerTask = new TimerTask() {
+    		public void run() {
+    			handler.post(new Runnable() {
+    				public void run() {
+    					nCounter++;
+    					shootSound();
+    				
+    					//update TextView
+    					if (hTextView != null) {
+    						hTextView.setText("Porks eaten: " + nCounter);
+    					} else {
+    						Log.d("ERROR", "hTextView is NULL");
+    					}
+    					Log.d("TIMER", "TimerTask run " + nCounter);
+    				}
+    			});		
+    		}};
+    		
+    		t.schedule(mTimerTask,  500,10000);
+    	}
+    	
+    		public void stopTask(){
+    			
+    			if(mTimerTask!=null) {
+    				if (hTextView != null) {
+    					hTextView.setText("Pork canceled after: " + nCounter + "photos");
+    				} else {
+    					Log.d("ERROR", "hTextView is NULL");
+    				}
+    			}
+    			Log.d("TIMER", "timer canceled");
+    			mTimerTask.cancel();
+    			
+    		}
+    		public void shootSound() {
+    			AudioManager meng = (AudioManager)
+getBaseContext().getSystemService(Context.AUDIO_SERVICE);
+    			int volume = meng.getStreamVolume(AudioManager.STREAM_NOTIFICATION);
+    			
+    			if (volume != 0) {
+    				if (_shootMP == null)
+    					_shootMP = MediaPlayer.create(getBaseContext(), Uri.parse("file:///system/media/audio/ui/camera_click.ogg"));
+    				if (_shootMP != null)
+    					_shootMP.start();
+    			}
+    		}
+    }
+
     
 
