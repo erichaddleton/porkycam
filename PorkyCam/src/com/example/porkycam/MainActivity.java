df4c09a4695fadf2f4cf3aa3006e474ddbb1bb93
Eric Haddleton <erichaddleton42@gmail.com>
2013-07-15 18:33:12 -0500

    For my sanity

--- PorkyCam/src/com/example/porkycam/MainActivity.java
+++ PorkyCam/src/com/example/porkycam/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.porkycam;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Timer;
@@ -9,13 +12,14 @@ import java.util.TimerTask;
 import android.app.Activity;
 import android.content.*;
 import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -34,6 +38,9 @@ public class MainActivity extends Activity {
 	Button hButton, hButtonStop;
 	private int nCounter = 0;
 	MediaPlayer _shootMP = null;
+	private Camera mCamera;
+	
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +48,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		hTextView = (TextView)findViewById(R.id.textView2);
 		hButton = (Button)findViewById(R.id.toggleButton1);
+		mCamera = getCameraInstance();
 	}
 	
 	//Get instance of camera object
@@ -62,11 +70,6 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    /*public void startCamera(View view) {
-    	Intent intent = new Intent(this, CameraOn.class);
-    	startActivity(intent);
-    }*/
-    
     public void onToggleClick(View view){
         // Is the toggle on?
         boolean on = ((ToggleButton) view).isChecked();
@@ -76,7 +79,32 @@ public class MainActivity extends Activity {
             stopTask();
         }
     }
-    public void doTimerTask()
+    
+    private PictureCallback mPicture = new PictureCallback() {
+
+	    
+		@Override
+	    public void onPictureTaken(byte[] data, Camera camera) {
+
+	        File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+	        if (pictureFile == null){
+	            //Log.d("PorkyCam", "Error creating media file, check storage permissions: " + e.getMessage());
+	            return;
+	        }
+
+	        try {
+	            FileOutputStream fos = new FileOutputStream(pictureFile);
+	            fos.write(data);
+	            fos.close();
+	        } catch (FileNotFoundException e) {
+	            Log.d("PorkyCam", "File not found: " + e.getMessage());
+	        } catch (IOException e) {
+	            Log.d("PorkyCam", "Error accessing file: " + e.getMessage());
+	        }
+	    }
+	};
+   
+	public void doTimerTask()
     {
     	Log.d("TIMER","TimerTask Started");
     	
@@ -86,7 +114,7 @@ public class MainActivity extends Activity {
     				public void run() {
     					nCounter++;
     					shootSound();
-    					dispatchTakePictureIntent(2);
+    					mCamera.takePicture(null, null, mPicture);
     					
     					//update TextView
     					if (hTextView != null) {
@@ -106,7 +134,7 @@ public class MainActivity extends Activity {
     			
     			if(mTimerTask!=null) {
     				if (hTextView != null) {
-    					hTextView.setText("Pork canceled after: " + nCounter + "photos");
+    					hTextView.setText("Pork canceled after: " + nCounter + " photos");
     				} else {
     					Log.d("ERROR", "hTextView is NULL");
     				}
@@ -128,7 +156,45 @@ getBaseContext().getSystemService(Context.AUDIO_SERVICE);
     			}
     		}
     		
-    		public static File getOutputMediaFile(int type){
+    		public static final int MEDIA_TYPE_IMAGE = 1;
+
+    		/** Create a file Uri for saving an image or video */
+    		private static Uri getOutputMediaFileUri(int type){
+    		      return Uri.fromFile(getOutputMediaFile(type));
+    		}
+
+    		/** Create a File for saving an image or video */
+    		private static File getOutputMediaFile(int type){
+    		    // To be safe, you should check that the SDCard is mounted
+    		    // using Environment.getExternalStorageState() before doing this.
+
+    		    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+    		              Environment.DIRECTORY_PICTURES), "PorkyCam");
+    		    // This location works best if you want the created images to be shared
+    		    // between applications and persist after your app has been uninstalled.
+
+    		    // Create the storage directory if it does not exist
+    		    if (! mediaStorageDir.exists()){
+    		        if (! mediaStorageDir.mkdirs()){
+    		            Log.d("PorkyCam", "failed to create directory");
+    		            return null;
+    		        }
+    		    }
+
+    		    // Create a media file name
+    		    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date(type));
+    		    File mediaFile;
+    		    if (type == MEDIA_TYPE_IMAGE){
+    		        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+    		        "IMG_"+ timeStamp + ".jpg");
+    		    } else {
+    		        return null;
+    		    }
+
+    		    return mediaFile;
+    		}
+    		
+    		/*public static File getOutputMediaFile(int type){
     			
     			File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "PorkyCam");
     			
@@ -153,13 +219,8 @@ getBaseContext().getSystemService(Context.AUDIO_SERVICE);
     			}
     			
     			return mediaFile;
-    		}
+    		}*/
     		
-    		protected void dispatchTakePictureIntent(int actionCode) {
-    			Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-    			//takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mediaFile));
-    			startActivityForResult(takePictureIntent, actionCode);
-    		}
     		
     }
 
